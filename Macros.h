101188f3da95c5cb3d5270762caee928eb23310f
cytpl <perfect_cripple@hotmail.com>
2016-05-29 23:01:05 +0300

    Create Macros.h

---
+++ Macros.h
@@ -0,0 +1,17 @@
+#ifndef __MACROS_H
+#define __MACROS_H
+
+#define GPIOInit   1
+//#define GPIOInit   0
+
+#define UARTInit   1
+//#define UARTInit   0
+
+#define TimerInit  1
+//#define TimerInit  0
+
+//#define SPIInit    1 
+#define SPIInit    0
+
+
+#endif

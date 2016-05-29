3a83d75a0c71c6aae845f37129a26855bd58b938
cytpl <perfect_cripple@hotmail.com>
2016-05-29 23:02:51 +0300

    Create USART.h

---
+++ USART.h
@@ -0,0 +1,11 @@
+#ifndef __USART_H
+#define __USART_H
+
+#include <iostm8s003f3.h>
+
+static void(*Usartcallback)(void);
+void UART_Init(void);
+void SendByte(char i);
+void SendString(const char *s);
+
+#endif

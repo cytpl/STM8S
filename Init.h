db5477a3c6a2c83ee03e2f6d0ad635691eb70073
cytpl <perfect_cripple@hotmail.com>
2016-05-29 23:00:40 +0300

    Create Init.h

---
+++ Init.h
@@ -0,0 +1,14 @@
+#ifndef __INIT_H
+#define __INIT_H
+
+#include "Macros.h"
+#include "GPIO.h"
+#include "TIMER4.h"
+#include "USART.h"
+#include "SPI.h"
+
+void Initalize(void);
+void Clock_Init(void);
+
+#endif
+

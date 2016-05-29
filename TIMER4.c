3324b5d81b65219ca861ca8914a8043b482853f1
cytpl <perfect_cripple@hotmail.com>
2016-05-29 22:58:48 +0300

    Create TIMER4.c

---
+++ TIMER4.c
@@ -0,0 +1,19 @@
+#include "TIMER4.h"
+
+long a;
+
+void TIMER4_Init(void)//1ms timer interrupt ayarı
+{
+  TIM4_CR1_ARPE = 1;
+  TIM4_IER_UIE  = 1; 
+  TIM4_PSCR     = 0x07; 
+  TIM4_ARR      = 0x7D; 
+  TIM4_CR1_CEN = 1;
+}
+
+#pragma vector=TIM4_OVR_UIF_vector
+__interrupt void Timer4_ISR(void)
+{
+  TIM4_SR_UIF = 0;
+  TIMcallback();
+}

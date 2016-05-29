3a9e99055c4dd96440c9025e8158772e77f1831b
cytpl <perfect_cripple@hotmail.com>
2016-05-29 22:57:42 +0300

    Create Init.c

---
+++ Init.c
@@ -0,0 +1,27 @@
+#include "Init.h"
+
+void Initalize(void)
+{
+  #if (SPIInit)
+    SPI_Init();
+  #endif
+    
+#if (GPIOInit)
+    GPIO_Init();
+#endif
+    
+#if (UARTInit)
+    UART_Init();
+#endif
+    
+#if (TimerInit)
+    TIMER4_Init();
+#endif
+}
+
+void Clock_Init(void)
+{
+   CLK_ICKR_HSIEN = 1;
+   while(!CLK_ICKR_HSIRDY);
+   CLK_CKDIVR = 0x00;
+}

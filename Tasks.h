18049a54bfd55b8344fd07e780627b9094abfed3
cytpl <perfect_cripple@hotmail.com>
2016-05-30 11:38:15 +0300

    Update Tasks.h

--- Tasks.h
+++ Tasks.h
@@ -2,9 +2,18 @@
 #define __TASKS_H
 
 #include <iostm8s003f3.h>
+#include "USART.h"
+#include "SPI.h"
+
+extern short Led,id;
+extern int Usarttime;
+extern int SPItime;
+extern char buffer[255];
 
 void TIMfunc(void);
-void USARTFunc(void);
+void USARTFunc(void); 
 void LED(void);
+void UsartTask(void);
+void SPITask(void);
 
 #endif
